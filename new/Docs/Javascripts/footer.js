65f9fbe0d4892e844e8d989b18082263b1ac75c6
hax0-r <talha18513@gmail.com>
2025-03-02 11:39:35 +0500

    new version added

---
+++ new/Docs/Javascripts/footer.js
@@ -0,0 +1,57 @@
+const footer = document.getElementById("footer");
+
+footer.innerHTML = `
+     <footer class="w-full mt-16 md:mt-28 bg-[#2c2b46] overflow-hidden">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <!--Grid-->
+                <div
+                    class="grid grid-cols-1 sm:grid-cols-3  gap-3 md:gap-8 py-10 max-sm:max-w-sm max-sm:mx-auto gap-y-8">
+                    <div class="mb-10 lg:mb-0">
+                        <a href="./index.html" class="flex justify-center lg:justify-start">
+                            <img src="./Docs/Images/logo.png" alt="logo" class=" h-32">
+                        </a>
+                    </div>
+                    <!--End Col-->
+                    <div class="lg:mx-auto text-left ">
+                        <h4 class="text-lg text-[#fff] font-medium mb-7">Contact Us</h4>
+                        <p class="text-gray-300 text-sm mb-5">123, Main Street, Anytown, USA</p>
+                        <ul class="text-sm  transition-all duration-500">
+                            <li class="mb-6 "><a href="tel:+447932574484"
+                                    class="text-gray-300 hover:text-gray-100 flex items-center gap-3"> <i
+                                        class="fa-solid fa-mobile-screen-button text-xl"></i> +44 7932574484</a></li>
+                            <li class="mb-6 "><a href="tel:+971585383851"
+                                    class="text-gray-300 hover:text-gray-100 flex items-center gap-3"> <i
+                                        class="fa-solid fa-mobile-screen-button text-xl"></i> +97158 538 3851</a></li>
+                            <li class="mb-6"><a href="mailto:Tradeflowmanagementservices@gmail.com"
+                                    class=" text-gray-300 hover:text-gray-100 flex items-center gap-3"><i
+                                        class="fa-solid fa-envelope text-xl"></i> Tradeflowmanagementservices@gmail.com</a></li>
+                        </ul>
+                    </div>
+                    <!--End Col-->
+                    <div class="lg:mx-auto text-left ">
+                        <h4 class="text-lg text-[#fff] font-medium mb-7">Connect With Us</h4>
+                        <ul class="text-sm  transition-all duration-500">
+                            <li class="mb-4 "><a href="#"
+                                class="text-gray-300 hover:text-gray-100 flex items-center gap-3"> <i
+                                    class="fa-brands fa-linkedin text-xl"></i> LinkedIn</a></li>
+                            <li class="mb-4 "><a href="#"
+                                class="text-gray-300 hover:text-gray-100 flex items-center gap-3"> <i
+                                    class="fa-brands fa-facebook text-xl"></i> Facebook</a></li>
+                            <li class="mb-4 "><a href="#"
+                                class="text-gray-300 hover:text-gray-100 flex items-center gap-3"> <i
+                                    class="fa-brands fa-instagram text-xl"></i> Instagram</a></li>
+                        </ul>
+                    </div>
+                  
+                </div>
+                <!--Grid-->
+                <div class="py-7 border-t border-gray-200">
+                    <div class="flex items-center justify-center">
+                        <span class="text-sm text-gray-400 ">©<a href="./index.html">Tradeflow Management Services</a> 2025, All rights
+                            reserved.</span>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+`
---
+++ new/Docs/Javascripts/navbar.js
@@ -0,0 +1,57 @@
+const navbar = document.getElementById("navbar");
+
+navbar.innerHTML = `
+<nav class="z-40 relative">
+                <div class="max-w-7xl text-white mx-auto flex justify-between items-center w-full p-5">
+                    <a href="./index.html">
+                        <img src="./Docs/Images/logo.png" alt="logo" class=" h-20">
+                    </a>
+                    <ul class="md:flex hidden items-center gap-7">
+                        <li><a href="./index.html"
+                                class="hover:text-[#cfe8ec] text-white transition-all duration-500">Home</a>
+                        </li> |
+                        <li><a href="./about-us.html"
+                                class="hover:text-[#cfe8ec] text-white transition-all duration-500">About
+                                Us</a>
+                        </li> |
+                        <li><a href="./contactUs.html"
+                                class="hover:text-[#cfe8ec] text-white transition-all duration-500">Contact
+                                Us</a>
+                        </li>
+                    </ul>
+                    <i id="mobileMenuBtn" class="fa-solid fa-bars md:hidden text-2xl cursor-pointer"></i>
+                </div>
+            </nav>
+
+            <div id="mobileMenu"
+                class="fixed right-0 top-0 bg-[#1c9fb5] max-w-full w-0 overflow-hidden h-screen z-50 transition-all duration-500">
+                <i id="mobileMenuCloseBtn"
+                    class="fa-solid text-white fa-x z-50 relative flex items-end justify-end p-5 text-2xl cursor-pointer"></i>
+                <ul class="flex relative z-50 flex-col p-10 gap-7">
+                    <li><a href="./index.html"
+                            class="hover:text-[#cfe8ec] text-white transition-all duration-500">Home</a>
+                    </li>
+                    <li><a href="./about-us.html"
+                            class="hover:text-[#cfe8ec] text-white transition-all duration-500">About
+                            Us</a>
+                    </li>
+                    <li><a href="./contactUs.html"
+                            class="hover:text-[#cfe8ec] text-white transition-all duration-500">Contact
+                            Us</a>
+                    </li>
+                </ul>
+            </div>
+`
+
+
+const mobileMenuBtn = document.getElementById("mobileMenuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+const mobileMenuCloseBtn = document.getElementById("mobileMenuCloseBtn");
+
+mobileMenuBtn.addEventListener("click", () => {
+    mobileMenu.style.width = "100%";
+});
+
+mobileMenuCloseBtn.addEventListener("click", () => {
+    mobileMenu.style.width = "0";
+});
